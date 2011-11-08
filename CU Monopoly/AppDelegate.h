4a8c494546d5b4fa17d1352e84bd403255688030
Jason Li <s1155000601@Jasons-MacBook-Air.local>
2011-11-08 18:44:35 +0800

    Initial Commit

---
+++ CU Monopoly/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  CU Monopoly
+//
+//  Created by Jason Li on 8/11/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
